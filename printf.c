837957443346639ddd39ffd5cc139f0be21fc2bb
AOUDAI <aoudair16031999@gmail.com>
2023-09-19 15:51:12 +0100

    Fix the betty style

--- printf.c
+++ printf.c
@@ -3,15 +3,27 @@
 #include <unistd.h>
 #include <stdio.h>
 
+/**
+ * print_char - Prints char to the standard output.
+ * @ap: The address of the char.
+ * @i: The number of the characters printed.
+ *
+ */
 void print_char(va_list ap, int *i)
 {
 	char c;
-	
+
 	c = (char)va_arg(ap, int);
 	write(1, &c, 1);
 	*i = *i + 1;
 }
 
+/**
+ * print_string - Prints a string to the standard output.
+ * @ap: A pointer to the string.
+ * @i: The number of characters printed.
+ *
+ */
 void print_string(va_list ap, int *i)
 {
 	char *str;
@@ -23,20 +35,31 @@ void print_string(va_list ap, int *i)
 	{
 		for (j = 0; str[j] != '\0'; j++)
 		{
-			 c = str[j];
-		 	write(1, &c, 1);
+			c = str[j];
+			write(1, &c, 1);
 			*i = *i + 1;
 		}
 	}
 }
 
+/**
+ * print_origin - Prints the origing char from the format argument to stdout.
+ * @c: The character.
+ * @i: The numbers of characters printed.
+ *
+ */
 void print_origin(char c, int *i)
 {
 	write(1, &c, 1);
 	*i = *i + 1;
 }
 
-
+/**
+ * _printf - produces output accouding th a format.
+ * @format: The format string.
+ *
+ * Return: The number of characters printed.
+ */
 int _printf(const char *format, ...)
 {
 	int i, count;
