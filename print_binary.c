fd458a23879ed1a6d83e84f7c1ea9575c471ab50
netweaver <developer.oduwa@gmail.com>
2023-09-20 00:10:33 +0100

    created a new file for binary to fix betty error

--- print_binary.c
+++ print_binary.c
@@ -4,7 +4,7 @@
  * print_binary - Prints a number of the binary form.
  * @num: The number.
  * @i: Pointer to the number of characters printed.
- *
+ * Return: void
  */
 void print_binary(int num, int *i)
 {
@@ -16,3 +16,4 @@ void print_binary(int num, int *i)
 	write(1, &c, 1);
 	*i = *i + 1;
 }
+
