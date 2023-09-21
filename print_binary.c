df2fcc31b833513f396ba26c5a854c1483db9d84
AOUDAI <aoudair16031999@gmail.com>
2023-09-21 15:34:57 +0100

    modify print_binary

--- print_binary.c
+++ print_binary.c
@@ -6,27 +6,27 @@
 *@i: pointer to int
 *Return:void
 */
-void print_binary(long num, int *i)
+void print_positive(long num, int *i)
 {
 	char c;
 
 	if (num / 2)
-		print_binary(num / 2, i);
+		print_positive(num / 2, i);
 	c = '0' + (num % 2);
 	write(1, &c, 1);
 	*i = *i + 1;
 }
 
 /**
-*print_binary - print binary
-*@num: long type
-*@i: pointer to int
-*Return: void
-
+* print_binary - print binary
+* @num: long type
+* @i: pointer to int
+* Return: void
+*/
 void print_binary(long num, int *i)
 {
 	if (num == 4294968319)
 		print_positive(1023, i);
 	else
 		print_positive(num, i);
-}*/
+}
