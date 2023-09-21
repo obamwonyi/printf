2f5e02b02226e105b524209e6bf82b77ed18a9ec
AOUDAI <aoudair16031999@gmail.com>
2023-09-21 12:58:28 +0100

    Fix some errors

--- printf.c
+++ printf.c
@@ -24,6 +24,25 @@ void number_handeler(char format, va_list ap, int *i)
 }
 
 /**
+ * specifier_checker - Checks if the format is one of number specifiers.
+ * @format: The format specifier.
+ *
+ * Return: 1 if the format matcahs one of number specifiers, otherwise 0.
+ */
+int specifier_checker(char format)
+{
+	char num_spec[7] = "idbuoxX";
+	int i;
+
+	for (i = 0; i < 7; i++)
+	{
+		if (format == num_spec[i])
+			return (1);
+	}
+	return (0);
+}
+
+/**
 *_printf - mimics the printf function
 *@format: the string to be formatted
 *Return: num of char printed
@@ -43,8 +62,9 @@ int _printf(const char *format, ...)
 			i++;
 			if (format[i] == '\0')
 				return (-1);
-			number_handeler(format[i], ap, &count);
-			if (format[i] == '%')
+			else if (specifier_checker(format[i]))
+				number_handeler(format[i], ap, &count);
+			else if (format[i] == '%')
 				print_origin(format[i], &count);
 			else if (format[i] == 'c')
 				print_char(ap, &count);
